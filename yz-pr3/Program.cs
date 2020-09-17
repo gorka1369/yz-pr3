464e210e0645aaf6a5599e7392f469af2ea98e90
gorka1369 <Tancor@icloud.com>
2020-09-17 15:36:30 +0300

    Добавьте файлы проекта.

---
+++ yz-pr3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace yz_pr3
+{
+    class Program
+    {
+        static bool IsPhone(string s)
+        {
+            return Regex.IsMatch(s, @" ^\+7[\s\(]?{ 3}[\s\)]? [\s\-]?d{3}[\-\s]?\d{2}[\-\s]?\d{2}$");
+        }
+        static bool IsZip(string s)
+        {
+            return Regex.IsMatch(s, @" ^\d{5}(\-\d{4})?$");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPhone("+7(926)269-25-11"));
+            
+
+        }
+    }
+}
